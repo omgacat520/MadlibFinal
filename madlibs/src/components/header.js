717e906033e16354699b0cb1a0adfb99ab25c533
Jake Ildefonso <jakejildefonso@gmail.com>
2018-07-27 01:23:59 -0600

    Edited SCSS file

--- madlibs/src/components/header.js
+++ madlibs/src/components/header.js
@@ -4,7 +4,7 @@ const Header = () => {
         return (
                 <div className="header">
                     <h1>Bottega Madlibs</h1>
-                    <p>Fill out hte fields below and click the generate button to see the Mad Lib story.</p>
+                    <p>Fill out the fields below and click the generate button to see the Mad Lib story.</p>
                 </div>
         )
 }
