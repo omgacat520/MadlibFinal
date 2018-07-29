b3d3272c4e9536092b685a733751232068e735b5
Jake Ildefonso <jakejildefonso@gmail.com>
2018-07-29 17:53:59 -0600

    set state on multiple inputs

--- madlibs/src/components/card.js
+++ madlibs/src/components/card.js
@@ -8,15 +8,16 @@ class Card extends Component {
         super()
 
         this.state = {
-            color: 'RED',
+            color: '',
             pluralNoun: ''
         }
         this.handleInputChange = this.handleInputChange.bind(this);
     }
     
     handleInputChange(event) {
-        console.log('this is the value', event.target.value);
-        this.setState({ color: event.target.value })
+        this.setState({ [event.target.name]: event.target.value })
+        console.log(event.target.name);
+        console.log(event.target.name.value);
     }
 
     render() {
@@ -24,15 +25,8 @@ class Card extends Component {
         return (
             <div className="card">
                 <h1>{this.state.color}</h1>
-                <input onChange={(event) => this.handleInputChange(event)}/>
-            </div>
-        );
-
-        return (
-            <div className="card">
-                <h1>{this.state.color}</h1>
-                {Input('Color')}
-                {Input('Plural Noun')}
+                {Input('Color', this.state.color, this.handleInputChange, 'color')}
+                {Input('Plural Noun', this.state.pluralNoun, this.handleInputChange, 'pluralNoun')}
             </div>
         )
     }
