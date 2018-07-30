a6c910d7cf9fc301f93411bb3cd2be134ffc63c5
Jake Ildefonso <jakejildefonso@gmail.com>
2018-07-29 22:53:24 -0600

    debugged the fuck outta this bitch

--- madlibs/src/components/card.js
+++ madlibs/src/components/card.js
@@ -14,8 +14,8 @@ class Card extends Component {
             adjectiveOne:'',//3
             celebOne: '',//4
             adjectiveTwo: '',//5
-            noun: '',//6
-            number: '',//7
+            nounOne: '',//6
+            numberOne: '',//7
             numberTwo: '',//8
             nounTwo: '',//9
             adjectiveThree: '',//10
@@ -38,7 +38,30 @@ class Card extends Component {
 
     handleFormSubmit(event) {
         event.preventDefault()
-        this.setState({contentVisible: !this.state.contentVisible})
+        if(this.state.contentVisible) {
+            this.setState ({
+                color: '',//1
+                pluralNoun: '',//2
+                adjectiveOne:'',//3
+                celebOne: '',//4
+                adjectiveTwo: '',//5
+                nounOne: '',//6
+                numberOne: '',//7
+                numberTwo: '',//8
+                nounTwo: '',//9
+                adjectiveThree: '',//10
+                celebTwo: '',//11
+                celebThree: '',//12
+                adjectiveFour: '', //13
+                nounThree: '', //14
+                celebFour: '', //15
+                adjectiveFive: '', //16 Done!
+                contentVisible:false,
+    
+            })
+        } else {
+            this.setState({contentVisible: true})
+        }
     }
     
     render() {
@@ -46,23 +69,23 @@ class Card extends Component {
         const inputData = [
             {title:'Color', state: this.state.color, name:'color'},//1
             {title:'Plural Noun', state: this.state.pluralNoun, name:'pluralNoun'},
-            {title:'Adjective', state: this.state.adjectiveOne, name:'adjective One'},
+            {title:'Adjective', state: this.state.adjectiveOne, name:'adjectiveOne'},
             {title:'Celebrity', state: this.state.celebOne, name:'celebOne'},//4 Finished!
 
-            {title:'Adjective', state: this.state.adjectiveTwo, name:'adjective Two'},//5
-            {title:'Noun', state: this.state.noun, name:'noun'},
-            {title:'Number', state: this.state.number, name:'number'},
+            {title:'Adjective', state: this.state.adjectiveTwo, name:'adjectiveTwo'},//5
+            {title:'Noun', state: this.state.nounOne, name:'nounOne'},
+            {title:'Number', state: this.state.numberOne, name:'numberOne'},
             {title:'Number', state: this.state.numberTwo, name:'numberTwo'},//8 Finished!
 
             {title:'Noun', state: this.state.nounTwo, name:'nounTwo'},//9
-            {title:'Adjective', state: this.state.AdjectiveThree, name:'Adjective Three'},
+            {title:'Adjective', state: this.state.adjectiveThree, name:'adjectiveThree'},
             {title:'Celebrity', state: this.state.celebTwo, name:'celebTwo'},
             {title:'Celebrity', state: this.state.celebThree, name:'celebThree'},//12 Finished!
 
-            {title:'Adjective', state: this.state.adjectiveFour, name:'adjective Four'},//13
+            {title:'Adjective', state: this.state.adjectiveFour, name:'adjectiveFour'},//13
             {title:'Noun', state: this.state.nounThree, name:'nounThree'},
             {title:'Celebrity', state: this.state.celebFour, name:'celebFour'},
-            {title:'Adjective', state: this.state.adjectiveFive, name:'adjective Five'},//16 Finished!
+            {title:'Adjective', state: this.state.adjectiveFive, name:'adjectiveFive'},//16 Finished!
         ]
 
 
@@ -75,7 +98,7 @@ class Card extends Component {
                 </div>
                 <button type="submit">{!this.state.contentVisible ? 'Generate Mad Lib' : 'Clear Form'}</button>
                 {
-                    this.state.contentVisible ? <Content data={!this.state}/> : '' //render nothing you cuck i s2g
+                    this.state.contentVisible ? <Content data={this.state}/> : '' //render nothing you cuck i s2g
                 }
             </form>
         )
