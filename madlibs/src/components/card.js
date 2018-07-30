53b4bc7b8bcd4f1c03a1f8ec810d4a7380469da0
Jake Ildefonso <jakejildefonso@gmail.com>
2018-07-30 03:17:48 -0600

    fuck dude just end it already

--- madlibs/src/components/card.js
+++ madlibs/src/components/card.js
@@ -98,7 +98,7 @@ class Card extends Component {
                         return Input( (data), this.handleInputChange, index) })
                     }
                 </div>
-                <button type="submit">{!this.state.contentVisible ? 'Generate Mad Lib' : 'Clear Form'}</button>
+                <button className={`card__${!this.state.contentVisible ? 'generate' : 'clear'}`} type="submit">{!this.state.contentVisible ? 'Generate Mad Lib' : 'Clear Form'}</button>
                 {
                     this.state.contentVisible ? <Content data={this.state}/> : '' //render nothing you cuck i s2g
                 }
