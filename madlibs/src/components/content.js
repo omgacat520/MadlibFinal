5478094c491a747892aadb7fb24be37b54da6c90
Jake Ildefonso <jakejildefonso@gmail.com>
2018-07-30 02:39:27 -0600

    fixed functionality and almost done with styles. End my suffering.

--- madlibs/src/components/content.js
+++ madlibs/src/components/content.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 class Content extends Component {
     render() {
         const data = this.props.data;
-        data.map((object,index) => {
-            console.log(index);
-        })
+        //     for(key in data) {
+        //         console.log(key)
+        //     }
+        //     //this.props.data.map((object,index) => {
+        //         console.log(index);
+        //     })
         
         return (
             <p>
